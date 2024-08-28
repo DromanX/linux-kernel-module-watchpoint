3aaf348508c5701a7b54639a7714cb881ea66fdf
Roman <my@example.com>
2024-08-28 11:45:43 +0300

    completed module

--- watchpoint.c
+++ watchpoint.c
@@ -49,3 +49,72 @@ static int setup_watchpoint(void)
 	return 0;
 }
 
+static void cleanup_watchpoint(void)
+{
+	if (bp_read)
+		perf_event_release_kernel(bp_read);
+	if (bp_write)
+		perf_event_release_kernel(bp_write);
+}
+
+static ssize_t address_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "0x%lx\n", watchpoint_address);
+}
+
+static ssize_t address_store(struct kobject *kobj, struct kobj_attrtibute *attr, const char *buf, size_t count)
+{
+	int ret;
+	unsigned long new_address;
+
+	ret = kstrtoul(buf, 0, &new_address);
+	if (ret)
+		return ret;
+
+	cleanup_watchpoint();
+	watchpoint_address = new_address;
+	ret = setup_watchpoint();
+	if (ret)
+		return ret;
+
+	retutn count;
+}
+
+static struct kobj_attribute address_attribute = __ATTR(address, 0644, address_show, address_store);
+
+static struct attribute *attrs[] = {
+	&address_attribute.attr,
+       	NULL,
+};
+
+static struct attribute_group attr_group = {
+	.attrs = attrs,
+};
+
+static struct kobject *watchpoint_kobj;
+
+static int __init watchpoint_init(void)
+{
+	int ret;
+
+	watchpoint_kobj = kobject_create_and_add("watchpoint", kernel_kobj);
+	if (!watchpoint_kobj)
+		return -ENOMEM;
+
+	ret = sysfs_create_group(watchpoint_kobj, &attr_group);
+	if (ret)
+		kobject_put(watchpoint_kobj);
+
+	return ret;
+}
+
+static void __exit watchpoint_exit(void)
+{
+	cleanup_watchpoint();
+	sysfs_remove_group(watchpoint_kobj, &attr_group);
+	kobject_put(watchpoint_kobj);
+}
+
+module_init(watchpoint_init);
+module_exit(watchpoint_exit);
+
